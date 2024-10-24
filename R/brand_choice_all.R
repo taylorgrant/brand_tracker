3157f5b112e8f54c85dc2ba2a92d744562de2624
taylorgrant <taylorgrant38@gmail.com>
2024-10-24 13:31:35 -0700

    to run across all brands for competitive

---
+++ R/brand_choice_all.R
@@ -0,0 +1,41 @@
+brand_choice_all <- function() {
+  # Define the brands to iterate over
+  brands <- c("Audi", "BMW", "Lexus", "Mercedes Benz", "Tesla")
+  
+  # Initialize empty tibbles for each category
+  all_brand_vars <- dplyr::tibble(var = character(), q = character(), brand = character())
+  all_key_attrs <- dplyr::tibble(var = character(), q = character(), brand = character())
+  all_brand_attrs <- dplyr::tibble(var = character(), q = character(), brand = character())
+  
+  # Iterate over each brand and accumulate the results
+  for (brand in brands) {
+    # Get the individual results for each brand
+    result <- brand_choice(brand)
+    
+    # Add a brand column to each tibble and accumulate
+    all_brand_vars <- dplyr::bind_rows(
+      all_brand_vars,
+      result$brand_vars %>% dplyr::mutate(brand = brand)
+    )
+    
+    all_key_attrs <- dplyr::bind_rows(
+      all_key_attrs,
+      result$key_attrs %>% dplyr::mutate(brand = brand)
+    )
+    
+    all_brand_attrs <- dplyr::bind_rows(
+      all_brand_attrs,
+      result$brand_attrs %>% dplyr::mutate(brand = brand)
+    )
+  }
+  
+  # Return a list of the three accumulated tibbles
+  list(
+    brand_vars = all_brand_vars,
+    key_attrs = all_key_attrs,
+    brand_attrs = all_brand_attrs
+  )
+}
+
+# Example usage
+result <- brand_choice_all()
