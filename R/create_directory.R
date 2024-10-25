d78875523c1d8ff7ee96d06e8449c221d392235b
taylorgrant <taylorgrant38@gmail.com>
2024-10-25 09:27:43 -0700

    adding new subfolder

--- R/create_directory.R
+++ R/create_directory.R
@@ -1,31 +1,27 @@
-create_directory <- function(brand, filters, main_subfolders = c("tables", "figures"), subfolders = c("Campaign", "Social", "Digital")) {
-  # if (is.null(filters)) {
-  #   # Create the main folder based on brand and date
-  #   main_path <- here::here(glue::glue("{brand}_{Sys.Date()}"))
-  #   if (!dir.exists(main_path)) {
-  #     dir.create(main_path, recursive = TRUE)
-  #   } 
-  # } else {
+create_directory <- function(brand, filters, main_subfolders = c("tables", "figures", "competitive"), subfolders = c("Campaign", "Social", "Digital")) {
+
     main_path <- here::here("processed",glue::glue("{brand}-{filters}_{Sys.Date()}"))
     if (!dir.exists(main_path)) {
       dir.create(main_path, recursive = TRUE)
     } 
   # }
     
-  # Create the main subfolders ("tables" and "figures")
+  # Create the main subfolders ("tables", "figures", and "competitive")
   purrr::walk(main_subfolders, function(main_subfolder) {
     main_subfolder_path <- file.path(main_path, main_subfolder)
     if (!dir.exists(main_subfolder_path)) {
       dir.create(main_subfolder_path)
     } 
     
-    # Create the subfolders ("Campaign", "Social", "Digital") inside each main subfolder
-    purrr::walk(subfolders, function(subfolder) {
-      subfolder_path <- file.path(main_subfolder_path, subfolder)
-      if (!dir.exists(subfolder_path)) {
-        dir.create(subfolder_path)
-      } 
-    })
+    # Only create subfolders for "tables" and "figures"
+    if (main_subfolder %in% c("tables", "figures")) {
+      purrr::walk(subfolders, function(subfolder) {
+        subfolder_path <- file.path(main_subfolder_path, subfolder)
+        if (!dir.exists(subfolder_path)) {
+          dir.create(subfolder_path)
+        }
+      })
+    }
   })
   return(main_path)
   }
